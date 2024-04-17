9e7195d36266f873445cd01dccb855337093900f
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-04-17 18:32:58 +0200

    feat : moovement

--- Assets/Script/MooveController.cs
+++ Assets/Script/MooveController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class MooveController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Camera mainCamera;
 
-    // Update is called once per frame
     void Update()
     {
+        Vector2 newPosition = transform.position;
         
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += Vector2.left * (speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += Vector2.right * (speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += Vector2.up * (speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += Vector2.down * (speed * Time.deltaTime);
+        }
+        
+        if (IsWithinCameraView(newPosition))
+        {
+            transform.position = newPosition;
+        }
+    }
+    
+    bool IsWithinCameraView(Vector2 position)
+    {
+        Vector2 viewportPosition = mainCamera.WorldToViewportPoint(position);
+        return viewportPosition.x >= 0.015 && viewportPosition.x <= 0.985 && viewportPosition.y >= 0.025 && viewportPosition.y <= 0.97;
     }
 }
