15d6d0da56ab982c3d73c3af4394990656acc925
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-04-17 21:36:39 +0200

    fix : coding style

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour
+namespace Script
 {
-    public static bool IsWithinCameraView(Vector2 position)
+    public class CameraManager : MonoBehaviour
     {
-        Vector2 viewportPosition = Camera.main.WorldToViewportPoint(position);
-        return viewportPosition.x >= 0.015 && viewportPosition.x <= 0.985 && viewportPosition.y >= 0.025 && viewportPosition.y <= 0.97;
+        public static bool IsWithinCameraView(Vector2 position)
+        {
+            Vector2 viewportPosition = Camera.main.WorldToViewportPoint(position);
+            return viewportPosition.x >= 0.015 && viewportPosition.x <= 0.985 && viewportPosition.y >= 0.025 &&
+                   viewportPosition.y <= 0.97;
+        }
     }
 }
